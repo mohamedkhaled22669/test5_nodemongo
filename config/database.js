d02dce820772c13ebcac056c62222288b454c8bb
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2019-10-01 14:40:56 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,2 @@
-var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/eventsDB', { useNewUrlParser: true }, function(err) {
 
-    if (err) {
-        console.log('err');
-    } else {
-        console.log('connected to db succesfuly...');
-    }
-
-});
